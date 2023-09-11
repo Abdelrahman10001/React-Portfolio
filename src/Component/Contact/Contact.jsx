0a92c55a6bb1095f8fa9f7607bf9491e9a985f9d
Abdelrahman0100 <abdosaad0900@gmail.com>
2023-09-11 10:07:51 +0300

    portfolio added

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 
 export default function Contact() {
+  useEffect(() => {
+    const inputElements = document.querySelectorAll("input");
+    const labelElements = document.querySelectorAll("label");
 
-
+    inputElements.forEach((input, index) => {
+      input.addEventListener("input", (e) => {
+        if (e.data != null) {
+          labelElements[index].classList.add("labelStyle");
+        } else {
+          labelElements[index].classList.remove("labelStyle");
+        }
+      });
+    });
+  });
 
   return (
     <>
@@ -18,33 +30,55 @@ export default function Contact() {
               </div>
             </div>
           </div>
-          <div className="allinput position-relative ">
-            <label className=" position-absolute " htmlFor="userName">userName</label>
-          <input
-            type="text"
-            id="userName"
-            className=" w-50 mx-auto mb-5"
-            placeholder="Your Name"
-          />
+          <div className=" form-Field mx-auto col-md-6">
+          <div className=" position-relative ">
+            <label className=" position-absolute " htmlFor="userName">
+              userName :
+            </label>
+            <input
+              type="text"
+              id="userName"
+              className=" w-100 mx-auto mb-5"
+              placeholder="Your Name"
+            />
           </div>
+          <div className=" position-relative ">
+            <label className=" position-absolute " htmlFor="userAge">
+            userAge :
+            </label>
+            <input
+              type="number"
+              id="userAge"
+              className=" w-100 mx-auto mb-5"
+              placeholder="Your Age"
+            />
+          </div>
+          <div className=" position-relative ">
+            <label className=" position-absolute " htmlFor="userEmail">
+            userEmail :
+            </label>
+            <input
+              type="email"
+              id="userEmail"
+              className=" w-100 mx-auto mb-5"
+              placeholder="Your Email"
+            />
+          </div>
+          <div className=" position-relative ">
+            <label className=" position-absolute " htmlFor="userPass">
+            userPassword :
+            </label>
+            <input
+              type="password"
+              id="userPass"
+              className=" w-100 mx-auto mb-5"
+              placeholder="Your Password"
+            />
+          </div>
+
           
-          <input
-            type="number"
-            className=" w-50 mx-auto my-5"
-            placeholder="Your Age"
-          />
-          <input
-            type="email"
-            className=" w-50 mx-auto my-5"
-            placeholder="YOur Email"
-          />
-          <input
-            type="password"
-            className=" w-50 mx-auto my-5"
-            placeholder="YOur Password"
-          />
-          <div className=" d-flex">
-            <button className="btn btn-success mx-auto">Send Message</button>
+          
+            <button className="btn btn-success btn-mm mx-auto">Send Message</button>
             {/* Can't touch any sass */}
           </div>
         </div>
--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import img1 from "../../imgs/port1.png";
+import img2 from "../../imgs/port2.png";
+import img3 from "../../imgs/port3.png";
 export default function Portfolio() {
+
+  const imgs=document.querySelectorAll('img')
+
+  useEffect(()=>{
+    imgs.forEach((imgS)=>{
+      imgS.addEventListener('mouseenter',()=>{
+        console.log('yay');
+      })
+    })
+  })
+
   return (
-    <div>Portfolio</div>
-  )
+    <>
+      <div className="contacts  row justify-content-center m-5 align-items-center">
+        <div className="container-fluid p-5 ">
+          <div className=" text-center  row align-items-center p-2  ">
+            <div className="container-fluid p-2 ">
+              <h1 className="pb-2 mt-4 toptext fw-bolder">
+                PORTFOLIO COMPONENT
+              </h1>
+              <div className="linenStar my-3 d-flex justify-content-center align-items-center ">
+                <div className="lineContact"></div>
+                <i className="starContact fa-solid fa-star mx-3"></i>
+                <div className="lineContact"></div>
+              </div>
+            </div>
+          </div>
+          <div className="workingImgs w-100 ">
+            <div className="row g-5">
+              <div className="col-lg-4 portImg col-md-6 ">
+                <img src={img1} className="w-100" alt="" />
+                <div className="imgLayer  justify-content-center align-items-center">
+                  <h2>Plus</h2>
+                </div>
+              </div>
+              <div className="col-lg-4 portImg col-md-6">
+                <img src={img2} className="w-100" alt="" />
+              </div>
+              <div className="col-lg-4 portImg col-md-6">
+                <img src={img3} className="w-100" alt="" />
+              </div>
+              <div className="col-lg-4 portImg col-md-6">
+                <img src={img1} className="w-100" alt="" />
+              </div>
+              <div className="col-lg-4 portImg col-md-6">
+                <img src={img2} className="w-100" alt="" />
+              </div>
+              <div className="col-lg-4 portImg col-md-6">
+                <img src={img3} className="w-100" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
